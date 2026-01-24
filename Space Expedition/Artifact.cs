086f66bc9e55f13389ef3bb487eb0aaf7bc6aa34
Rone <Gamewarrior1andbowser@gmail.com>
2026-01-24 10:47:30 -0600

    Started project and added project bones.

--- Space Expedition/Artifact.cs
+++ Space Expedition/Artifact.cs
@@ -1,41 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Space_Expedition {
+﻿namespace Space_Expedition {
     internal class Artifact {
-        public string Name { get; set; }
-        public string EncodedName { get; }
-        public string Artist { get; }
-        public string Year { get; }
-        public string Log { get;  }
 
-        public bool IsEncoded { get; set; }
-
-        public Artifact(string name, string artist, string year, string log) {
-            EncodedName = name;
-            Artist = artist;
-            Year = year;
-            Log = log;
-        }
-
-        public void Decode() {
-            if (IsEncoded == false || Name.Length == EncodedName.Length/2) {
-                IsEncoded = false;
-            } else {
-
-                Decode();
-            }
-        }
-
-        public string Print() {
-            return $"{Name}, {Artist}, {Year}, {Log}";
-        }
-
-        public string Save() {
-            return $"{EncodedName} | {Artist} | {Year} | {Log}";
-        }
     }
 }
