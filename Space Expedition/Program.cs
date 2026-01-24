8efe74d10c8ad2a16e6dc87fab7a426f6c88073e
Rone <Gamewarrior1andbowser@gmail.com>
2026-01-24 12:46:21 -0600

    Added the missing 'internal class Program'

--- Space Expedition/Program.cs
+++ Space Expedition/Program.cs
@@ -3,27 +3,30 @@ using static System.IO.Directory;
 
 namespace Space_Expedition
 {
-    static void Main(string[] args) {
-        Bootup();
-    }
-
-    static void Bootup() {
-        if (!File.Exists("C:\\TempData\\Galatic_Vault.txt")) {
-            CreateDirectory("C:\\TempData");
-            Create("C:\\TempData\\Galatic_Vault.txt");
-            Console.WriteLine("New Data has been created in: \"C:\\TempData\\Galatic_Vault.txt\"");
-            //Add a continue here
-            try {
-                WriteAllText("C:\\TempData\\Galatic_Vault.txt", "Hopeium | Meme World | 2021 | A rainbow colored crystal filled with immense power, it only appears when all hope seems lost.");
-            } catch (System.IO.IOException) {
-                Console.WriteLine("Error, failed to preset file.");
-                Console.WriteLine("Please open \"C:\\TempData\\Galatic_Vault.txt\" and paste \"Hopeium | Meme World | 2021 | A rainbow colored crystal filled with immense power, it only appears when all hope seems lost.\" into the text file after you close the application.");
-                Console.WriteLine("");
-                Console.WriteLine("Then reboot the Space Expedition tracker!");
-                Console.WriteLine("");
-            }
-        } else {
-            //start the user interface here
+    internal class Program {
+        static void Main(string[] args) {
+            Bootup();
         }
+
+        static void Bootup() {
+            if (!File.Exists("C:\\TempData\\Galatic_Vault.txt")) {
+                CreateDirectory("C:\\TempData");
+                Create("C:\\TempData\\Galatic_Vault.txt");
+                Console.WriteLine("New Data has been created in: \"C:\\TempData\\Galatic_Vault.txt\"");
+                //Add a continue here
+                try {
+                    WriteAllText("C:\\TempData\\Galatic_Vault.txt", "Hopeium | Meme World | 2021 | A rainbow colored crystal filled with immense power, it only appears when all hope seems lost.");
+                } catch (System.IO.IOException) {
+                    Console.WriteLine("Error, failed to preset file.");
+                    Console.WriteLine("Please open \"C:\\TempData\\Galatic_Vault.txt\" and paste \"Hopeium | Meme World | 2021 | A rainbow colored crystal filled with immense power, it only appears when all hope seems lost.\" into the text file after you close the application.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Then reboot the Space Expedition tracker!");
+                    Console.WriteLine("");
+                }
+            } else {
+                //start the user interface here
+            }
+        }
+
     }
 }
