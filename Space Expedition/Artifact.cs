9a8b7cc482bee27c3fd0a6e16362d2a984e0bacc
Rone <Gamewarrior1andbowser@gmail.com>
2026-01-24 11:47:30 -0600

    Got an Artifact class started to make loading, decoding and saving artifacts easier.

--- Space Expedition/Artifact.cs
+++ Space Expedition/Artifact.cs
@@ -1,5 +1,35 @@
 ﻿namespace Space_Expedition {
     internal class Artifact {
+        public string Name { get; set; }
+        public string EncodedName { get; }
+        public string Artist { get; }
+        public string Year { get; }
+        public string Log { get; }
 
+        public bool IsEncoded { get; set; }
+
+        public Artifact(string name, string artist, string year, string log) {
+            EncodedName = name;
+            Artist = artist;
+            Year = year;
+            Log = log;
+        }
+
+        public void Decode() {
+            if (IsEncoded == false || Name.Length == EncodedName.Length / 2) {
+                IsEncoded = false;
+            } else {
+
+                Decode();
+            }
+        }
+
+        public string Print() {
+            return $"{Name}, {Artist}, {Year}, {Log}";
+        }
+
+        public string Save() {
+            return $"{EncodedName} | {Artist} | {Year} | {Log}";
+        }
     }
 }
